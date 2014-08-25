2116fe58afe60319ab2d4de48350fb86097d6a6d
Paul Adamson <paul.adamson.01@gmail.com>
2014-08-25 04:55:27 -0600

    Chapter 7 exercises complete

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -25,9 +25,76 @@ describe "User pages" do
 
     let(:submit) { "Create my account" }
 
-    describe "with invalid information" do
+    describe "with no information" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
+      end
+
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_title('Sign up') }
+        it { should have_content('error') }
+      end
+    end
+
+    describe "with invalid email" do
+      before do
+        fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "user@invalid"
+        fill_in "Password",     with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+      end
+
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_title('Sign up') }
+        it { should have_content('error') }
+      end
+    end
+
+    describe "with invalid password" do
+      before do
+        fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "user@invalid.com"
+        fill_in "Password",     with: "foo"
+        fill_in "Confirmation", with: "foo"
+      end
+
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_title('Sign up') }
+        it { should have_content('error') }
+      end
+    end
+
+    describe "with unmatched passwords" do
+      before do
+        fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "user@invalid.com"
+        fill_in "Password",     with: "foobar"
+        fill_in "Confirmation", with: "barfoo"
+      end
+
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_title('Sign up') }
+        it { should have_content('error') }
       end
     end
 
@@ -41,6 +108,14 @@ describe "User pages" do
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
     end
   end
